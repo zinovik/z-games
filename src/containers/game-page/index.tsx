2d780d3fe4a2aa11696b96b6ea2eee95f6b35c7c
Max Zinovik <zinovik@gmail.com>
2019-04-11 22:22:31 +0300

    fix mobile styles, update fix nimmt

--- src/containers/game-page/index.tsx
+++ src/containers/game-page/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Dispatch, bindActionCreators } from 'redux';
 import { Paper } from '@material-ui/core';
-import { GAME_STARTED } from 'z-games-base-game';
+import { GAME_NOT_STARTED } from 'z-games-base-game';
 
 import { GameInfo } from '../../components/game-info';
 import { GameTable } from '../../components/game-table';
@@ -37,7 +37,7 @@ function GamePagePure({ currentUser, game, isButtonsDisabled, closeGame, leaveGa
   return (
     <main>
       <div className='game-page-container'>
-        <div className={`game-page-table${game.state === GAME_STARTED ? '' : ' game-page-table-not-started'}`}>
+        <div className={`game-page-table${game.state === GAME_NOT_STARTED ? 'game-page-table-not-started' : ''}`}>
           <GameTable
             game={game}
             currentUser={currentUser}
